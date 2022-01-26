ac20792ed88fb906beb14a31e122237ca77987d2
Joanna Lee <joanna.lee56@gmail.com>
2022-01-26 11:32:30 -0800

    Add Google Maps to general zip code of classifieds page

--- static/js/classified_details.js
+++ static/js/classified_details.js
@@ -1,50 +1,74 @@
 'use strict';
 
-// function initMap() {
-//   const sfBayCoords = {
-//     lat: 37.601773,
-//     lng: -122.20287,
-//   };
-
-//   const basicMap = new google.maps.Map(document.querySelector('#map'), {
-//     center: sfBayCoords,
-//     zoom: 12,
-//   });
-
-//   }
-
-
 function initMap() {
-    
-    const map = new google.maps.Map(document.querySelector('#map'), {
-      zoom: 7
+  
+  const postalCode = document.querySelector('.postal_code').innerText;
+  const geocoder = new google.maps.Geocoder();
+  geocoder.geocode({address: postalCode}, (results, status) => {
+      if (status === 'OK') {
+      // Get the coordinates of the user's location
+      const userLocation = results[0].geometry.location;
+      
+      const map = new google.maps.Map(document.querySelector('#map'), {
+        center: userLocation,
+        zoom: 18,
       });
-    const postalCode = document.querySelector('.postal_code').innerText;
-    // Could you reference jinja {{ classified.postal_code }}?
 
-    const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({address: postalCode}, (results, status) => {
-        if (status === 'OK') {
-        // Get the coordinates of the user's location
-        const userLocation = results[0].geometry.location;
-        
-        console.log(results)
-        console.log(userLocation)
-        
-        // Create a marker
-        new google.maps.Marker({
-            position: userLocation,
-            map,
-        });
-        
-        // Zoom in on the geolocated location
-        // map.setCenter(userLocation);
-        map.setZoom(18);
-        } else {
-        alert(`Geocode was unsuccessful for the following reason: ${status}`);
-        }
-    });
+      console.log(results)
+      console.log(userLocation)
+      
+      // Create a marker
+      // new google.maps.Marker({
+      //     position: userLocation,
+      //     map,
+      // });
+
+      // Zoom in on the geolocated location
+      map.setCenter(userLocation);
+      map.setZoom(15);
+      } else {
+      alert(`Geocode was unsuccessful for the following reason: ${status}`);
+      }
+  });
 }
+
+
+// function initMap() {
+
+//     const map = new google.maps.Map(document.querySelector('#map'), {
+//       center: {
+//         lat: 37.601773,
+//         lng: -122.20287,
+//       },
+//       zoom: 18
+//       });
+//     const postalCode = document.querySelector('.postal_code').innerText;
+//     // Could you reference jinja {{ classified.postal_code }}?
+
+//     const geocoder = new google.maps.Geocoder();
+//     geocoder.geocode({address: postalCode}, (results, status) => {
+//         if (status === 'OK') {
+//         // Get the coordinates of the user's location
+//         const userLocation = results[0].geometry.location;
+        
+//         console.log(results)
+//         console.log(userLocation)
+        
+//         // Create a marker
+//         new google.maps.Marker({
+//             position: userLocation,
+//             map,
+//         });
+        
+//         // Zoom in on the geolocated location
+//         // map.setCenter(userLocation);
+//         map.setZoom(18);
+//         } else {
+//         alert(`Geocode was unsuccessful for the following reason: ${status}`);
+//         }
+
+//     });
+// }
 
 // function initMap() {
 //     const map = new google.maps.Map(document.querySelector('#map'), {
